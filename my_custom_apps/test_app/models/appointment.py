67a190e0383646f4f4024dd433a97de68009edc3
Md. Hossain AKash <mdakash41@gmail.com>
2021-12-09 18:52:47 +0600

    lecture18

---
+++ my_custom_apps/test_app/models/appointment.py
@@ -0,0 +1,23 @@
+from odoo import models,fields,api,_
+
+
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'
+    _description = 'Appointment'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _order = 'id desc'
+
+    @api.model
+    def create(self, vals_list):
+        if vals_list.get('name',_('New')) == _("New"):
+            vals_list['name'] = self.env['ir.sequence'].next_by_code('hostpital.appointment') or _('New')
+        result = super(HospitalAppointment, self).create(vals_list)
+        return result
+
+    name = fields.Char(string='Appointment ID', required = True, copy = False, readonly = True,
+                       index = True, default= lambda self:_('New')
+                       )
+    patient_id = fields.Many2one('hospital.patient',string="Patient",required = True)
+    patient_age = fields.Integer("Age", related='patient_id.patient_age')
+    notes = fields.Text(String= 'Registration Note')
+    appointment_date = fields.Date(string="Date",required=True)
---
+++ my_custom_apps/test_app/models/models.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+
+    patient_name = fields.Char(string="Patient Name")
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Patient Record"
+    _rec_name = 'name'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    @api.depends('patient_age')
+    def getAgeGroup(self):
+        for record in self:
+            if record.patient_age < 18:
+                record.age_group = 'minor'
+                return
+            record.age_group = 'major'
+
+    @api.constrains('patient_age')
+    def checkAge(self):
+        for record in self:
+            if record.patient_age < 5:
+                raise ValidationError("Age should be greater than 5")
+
+    name = fields.Char("Name", required=True)
+    gender = fields.Selection([('male','Male'),('fe_male','Female')],default='male',string='Gender', track_visibility='always')
+    patient_age = fields.Integer("Age", track_visibility='always')
+    notes = fields.Text(string="Notes")
+    image = fields.Binary(string="Image")
+    name_seq = fields.Char(string='Order Reference', required=True, copy=False, readonly=True, index=True, default= lambda self:_('New'))
+    age_group = fields.Selection(
+        [
+            ('major','Major'),
+            ('minor','Minor')
+        ],
+        string="Age Group", compute="getAgeGroup"
+    )
+    @api.model
+    def create(self, vals):
+        if vals.get('name_seq',_('New'))==_('New'):
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code('hospital.patient.sequence') or _('New')
+            result = super(HospitalPatient, self).create(vals)
+            return result
+
+
+# class test_app(models.Model):
+#     _name = 'test_app.test_app'
+#     _description = 'test_app.test_app'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
